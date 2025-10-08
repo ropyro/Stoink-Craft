07ae830657bce4dcd1237b13053cf3b7c506967c
ropyro <roninrichman@gmail.com>
2025-10-08 01:30:44 -0700

    Updated create command to reflect founding cost.

--- src/main/java/com/stoinkcraft/commands/enterprisecmd/subcommands/CreateSubCommand.java
+++ src/main/java/com/stoinkcraft/commands/enterprisecmd/subcommands/CreateSubCommand.java
@@ -1,11 +1,17 @@
 package com.stoinkcraft.commands.enterprisecmd.subcommands;
 
+import com.stoinkcraft.StoinkCore;
 import com.stoinkcraft.commands.SubCommand;
 import com.stoinkcraft.enterprise.Enterprise;
 import com.stoinkcraft.enterprise.EnterpriseManager;
+import com.stoinkcraft.utils.ChatUtils;
+import com.stoinkcraft.utils.SCConstants;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class CreateSubCommand implements SubCommand {
 
@@ -35,9 +41,22 @@ public class CreateSubCommand implements SubCommand {
             return;
         }
 
+        if(StoinkCore.getEconomy().getBalance(player) < SCConstants.ENTERPRISE_FOUNDING_COST){
+            player.sendMessage("Insufficient funds. Founding enterprise costs: " + ChatUtils.formatMoney(SCConstants.ENTERPRISE_FOUNDING_COST));
+            return;
+        }
+
+//        StringBuilder nameBuilder = new StringBuilder(args[1]);
+//        List<String> argsList = Arrays.stream(args).toList();
+//        argsList.remove(0);
+//        argsList.forEach(a -> nameBuilder.append(" " + a));
+
         String name = args[1];
         Enterprise e = new Enterprise(name, player.getUniqueId());
         EnterpriseManager.getEnterpriseManager().createEnterprise(e);
+
+        StoinkCore.getEconomy().withdrawPlayer(player, SCConstants.ENTERPRISE_FOUNDING_COST);
+
         player.sendMessage(ChatColor.GREEN + "Enterprise '" + name + "' created.");
     }
 }
