9fe9affdbade611e7792c0565ecc4a3ad7ef6a67
ropyro <roninrichman@gmail.com>
2025-11-26 00:21:46 -0800

    Chat listener updates

---
+++ src/main/java/com/stoinkcraft/enterprise/listeners/PlayerChatListener.java
@@ -0,0 +1,28 @@
+package com.stoinkcraft.enterprise.listeners;
+
+import com.stoinkcraft.StoinkCore;
+import com.stoinkcraft.enterprise.listeners.chatactions.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.*;
+
+public class PlayerChatListener implements Listener {
+
+    private StoinkCore plugin;
+    private List<ChatAction> chatActionList = new ArrayList<>();
+
+    public PlayerChatListener(StoinkCore plugin){
+        this.plugin = plugin;
+        chatActionList.add(new ChatDepositAction(plugin));
+        chatActionList.add(new ChatInvestAction(plugin));
+        chatActionList.add(new ChatWithdrawAction(plugin));
+        chatActionList.add(new EnterpriseChatAction(plugin));
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        chatActionList.stream().forEach(a -> a.handleChat(event));
+    }
+}
