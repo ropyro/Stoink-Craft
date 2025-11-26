78d61b7490ff2f70a19b5ab19c53b797720811bb
ropyro <roninrichman@gmail.com>
2025-11-26 00:21:34 -0800

    Enterprise MOTD added to general player join listener

--- src/main/java/com/stoinkcraft/enterprise/listeners/PlayerJoinListener.java
+++ src/main/java/com/stoinkcraft/enterprise/listeners/PlayerJoinListener.java
@@ -1,8 +1,12 @@
 package com.stoinkcraft.enterprise.listeners;
 
 import com.stoinkcraft.StoinkCore;
+import com.stoinkcraft.enterprise.Enterprise;
 import com.stoinkcraft.enterprise.EnterpriseManager;
 import com.stoinkcraft.enterprise.guis.UnemployedGUI;
+import com.stoinkcraft.misc.daily.DailyManager;
+import com.stoinkcraft.utils.ChatUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,5 +35,28 @@ public class PlayerJoinListener implements Listener {
             int delayTicks = 30 * 20;
             task.runTaskLater(plugin, delayTicks);
         }
+        Bukkit.getScheduler().runTaskLater(StoinkCore.getInstance(), new Runnable() {
+            @Override
+            public void run() {
+                sendMOTD(event.getPlayer());
+            }
+        }, 20*3);
+    }
+
+    private void sendMOTD(Player player){
+        Enterprise enterprise = EnterpriseManager.getEnterpriseManager().getEnterpriseByMember(player.getUniqueId());
+
+        player.sendMessage("§7§m                                                                         §r");
+        player.sendMessage("           §b§lWelcome to Stonk Craft!");
+        player.sendMessage(" ");
+        if(DailyManager.INSTANCE.canClaimDaily(player))
+            player.sendMessage(" §a• §fYour daily reward is available §b/daily");
+        if(enterprise == null)
+            player.sendMessage(" §a• §fYou are unemployed! Do §b/enterprise §fto get a job!");
+        else
+            player.sendMessage(" §a• §b"+ enterprise.getName() + " §fhas increased in value \n   from §a$" +
+                    ChatUtils.formatMoney(enterprise.getPriceHistory().getFirst().getSharePrice())
+                    + "§f to §a$" + ChatUtils.formatMoney(enterprise.getShareValue()));
+        player.sendMessage("§7§m                                                                         §r");
     }
 }
