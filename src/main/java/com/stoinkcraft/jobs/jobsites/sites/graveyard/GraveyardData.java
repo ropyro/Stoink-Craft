b5e8c7390beb800e7bc8bdd7881d81e62be78251
ropyro <roninrichman@gmail.com>
2026-01-05 18:55:23 -0800

    Added Graveyard jobsite

---
+++ src/main/java/com/stoinkcraft/jobs/jobsites/sites/graveyard/GraveyardData.java
@@ -0,0 +1,10 @@
+package com.stoinkcraft.jobs.jobsites.sites.graveyard;
+
+import com.stoinkcraft.jobs.jobsites.JobSite;
+import com.stoinkcraft.jobs.jobsites.JobSiteData;
+
+public class GraveyardData extends JobSiteData {
+    public GraveyardData(boolean isBuilt, JobSite parent) {
+        super(isBuilt, parent);
+    }
+}
--- src/main/java/com/stoinkcraft/jobs/jobsites/sites/graveyard/GraveyardSite.java
+++ src/main/java/com/stoinkcraft/jobs/jobsites/sites/graveyard/GraveyardSite.java
@@ -1,25 +1,52 @@
 package com.stoinkcraft.jobs.jobsites.sites.graveyard;
 
+import com.stoinkcraft.StoinkCore;
 import com.stoinkcraft.enterprise.Enterprise;
 import com.stoinkcraft.jobs.jobsites.JobSite;
 import com.stoinkcraft.jobs.jobsites.JobSiteData;
 import com.stoinkcraft.jobs.jobsites.JobSiteType;
+import com.stoinkcraft.jobs.jobsites.components.JobSiteHologram;
+import com.stoinkcraft.jobs.jobsites.sites.skyrise.SkyriseData;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.util.Vector;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GraveyardSite extends JobSite {
-    public GraveyardSite(Enterprise enterprise, JobSiteType type, Location spawnPoint, File schematic, JobSiteData data, boolean isBuilt) {
-        super(enterprise, type, spawnPoint, schematic, data, isBuilt);
+
+    private JobSiteHologram entryHologram;
+
+    public GraveyardSite(Enterprise enterprise, Location spawnPoint, GraveyardData data) {
+        super(enterprise, JobSiteType.GRAVEYARD, spawnPoint,
+                new File(StoinkCore.getInstance().getDataFolder(), "/schematics/graveyard.schem"),
+                data, data.isBuilt());
+
+        String entryHologramName = enterprise.getID() + "_" + JobSiteType.GRAVEYARD.name() + "_entryway";
+        entryHologram = new JobSiteHologram(this, entryHologramName, new Vector(-3.5, 3, 0.5), getEntryHoloLines());
     }
 
     @Override
     public void build() {
-
+        super.build();
     }
 
     @Override
     public void tick() {
+        super.tick();
+    }
 
+    private List<String> getEntryHoloLines(){
+        List<String> entryHoloGramLines = new ArrayList<>();
+        entryHoloGramLines.add(ChatColor.AQUA + "" + ChatColor.BOLD + enterprise.getName() + "'s");
+        entryHoloGramLines.add(ChatColor.AQUA + "" + ChatColor.BOLD + "Graveyard");
+        return entryHoloGramLines;
+    }
+
+    @Override
+    public GraveyardData getData() {
+        return (GraveyardData) super.getData();
     }
 }
